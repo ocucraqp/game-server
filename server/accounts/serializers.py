4e04311f9a3eea9cd48c703c15f24dad2f25fdaa
Masaya Oura <mairu0514m.o@gmail.com>
2020-05-17 14:03:38 +0900

    コード整理

--- server/accounts/serializers.py
+++ server/accounts/serializers.py
@@ -21,20 +21,27 @@ class StatusSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         user = self.instance
+
         pre_st_point = user.st_point
         pre_st_hp = user.status_hp
         pre_st_arm = user.status_arm
         pre_st_luck = user.status_luck
+
+        new_st_point = data.get('st_point')
         new_st_hp = data.get('status_hp')
         new_st_arm = data.get('status_arm')
         new_st_luck = data.get('status_luck')
-        new_st_point = data.get('st_point')
+
         if pre_st_hp > new_st_hp or pre_st_arm > new_st_arm or pre_st_luck > new_st_luck:
             raise serializers.ValidationError('ステータスが下がっています')
+
         if new_st_point > pre_st_point:
             raise serializers.ValidationError('ステータスポイントが増えています')
+
         if pre_st_point + pre_st_hp + pre_st_arm + pre_st_luck != new_st_point + new_st_hp + new_st_arm + new_st_luck:
             raise serializers.ValidationError('ステータスポイントが正しく振り分けられていません')
+
         if new_st_point < 0:
             raise serializers.ValidationError('ステータスポイントが負になっています')
+
         return data
